77a62da6d4807de29cc82eb15aff8922a8948ede
gammaSpeck <madhukm3141@gmail.com>
2026-02-14 13:51:32 +0530

    fix(analysis): modularized analysis page and changed analysis section order - Summary section is moved above graph

---
+++ src/components/analysis/CustomPieTooltip.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useCurrency } from "@/contexts/CurrencyContext";
+
+type Props = {
+  active?: boolean;
+  payload?: any;
+};
+
+export default function CustomPieTooltip({ active, payload }: Props) {
+  const { currency } = useCurrency();
+
+  if (!active || !payload || !payload?.length) return null;
+
+  const data = payload[0];
+  const percentage = ((data.value / data.payload.total) * 100).toFixed(1);
+  return (
+    <div
+      className="px-3 py-2 rounded-lg shadow-lg border border-white"
+      style={{
+        backgroundColor: data.payload.color,
+      }}
+    >
+      <p className="font-medium text-white">{data.name}</p>
+      <p className="text-white/90">
+        {currency.symbol}
+        {data.value.toLocaleString(currency.locale)}
+      </p>
+      <p className="text-white/80 text-sm">{percentage}%</p>
+    </div>
+  );
+}
